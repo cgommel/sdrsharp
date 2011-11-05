218066bde6abcce88b6645464793fc21be5163cd
prog <prog@d3cde5d6-82b8-4669-8b62-43054d6781d8>
2011-11-05 20:38:13 +0000

    Added DC Offset property
    git-svn-id: https://subversion.assembla.com/svn/sdrsharp/trunk@3 d3cde5d6-82b8-4669-8b62-43054d6781d8

--- Radio/DcRemover.cs
+++ Radio/DcRemover.cs
@@ -10,6 +10,11 @@
             _ratio = ratio;
         }
 
+        public double Offset
+        {
+            get { return _mean; }
+        }
+
         public void Process(double[] buffer)
         {
             for (var i = 0; i < buffer.Length; i++)
