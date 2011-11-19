094ed0aa4437c2367238429c5deb3e470eaae787
prog <prog@d3cde5d6-82b8-4669-8b62-43054d6781d8>
2011-11-19 14:06:06 +0000

    Forgotten file
    git-svn-id: https://subversion.assembla.com/svn/sdrsharp/trunk@45 d3cde5d6-82b8-4669-8b62-43054d6781d8

---
+++ Radio/IFrontendController.cs
@@ -0,0 +1,10 @@
+﻿namespace SDRSharp.Radio
+{
+    public interface IFrontendController
+    {
+        void Open();
+        void Close();
+        bool IsOpen { get; }
+        int Frequency { get; set; }
+    }
+}
