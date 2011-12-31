9b8b932cfc918c1f7ab1a59e50ef0191016cc20e
prog <prog@d3cde5d6-82b8-4669-8b62-43054d6781d8>
2011-12-31 12:23:56 +0000

    Fixed a minor center frequency change regression
    git-svn-id: https://subversion.assembla.com/svn/sdrsharp/trunk@100 d3cde5d6-82b8-4669-8b62-43054d6781d8

--- SDRSharp/MainForm.cs
+++ SDRSharp/MainForm.cs
@@ -421,7 +421,7 @@ namespace SDRSharp
             frequencyNumericUpDown.Minimum = newCenterFreq - _vfo.SampleRate / 2;
             frequencyNumericUpDown.Value = newCenterFreq + _vfo.Frequency;
 
-            if (_frontendController != null && frontEndComboBox.Enabled)
+            if (_frontendController != null)
             {
                 _frontendController.Frequency = newCenterFreq;
             }
