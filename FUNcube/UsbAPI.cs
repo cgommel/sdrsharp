b509d3fc713888d55b15bff45062ff54fe6cc855
prog <prog@d3cde5d6-82b8-4669-8b62-43054d6781d8>
2011-12-31 10:24:58 +0000

    Enhanced FUNcube dongle code
    git-svn-id: https://subversion.assembla.com/svn/sdrsharp/trunk@97 d3cde5d6-82b8-4669-8b62-43054d6781d8

--- FUNcube/UsbAPI.cs
+++ FUNcube/UsbAPI.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace SDRSharp.FUNcube
 {
@@ -7,60 +8,87 @@ namespace SDRSharp.FUNcube
     {
         public const int DigcfInterfaceDevice = 0x00000010;
         public const int DigcfPresent = 0x00000002;
+        public const int SpdrpHardwareId = 0x00000001;
+
         public const uint GenericRead = 0x80000000;
         public const uint GenericWrite = 0x40000000;
         public const uint FileShareRead = 0x00000001;
         public const uint FileShareWrite = 0x00000002;
         public const int OpenExisting = 3;
 
-        [DllImport("setupapi.dll")]
+        [DllImport("setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool SetupDiGetDeviceRegistryProperty(
+            IntPtr deviceInfoSet,
+            ref SpDevInfoData deviceInfoData,
+            uint property,
+            out UInt32 propertyRegDataType,
+            StringBuilder propertyBuffer,
+            uint propertyBufferSize,
+            out UInt32 requiredSize);
+
+        [DllImport("setupapi.dll", SetLastError = true)]
+        public static extern bool SetupDiEnumDeviceInfo(
+            IntPtr deviceInfoSet,
+            uint memberIndex,
+            ref SpDevInfoData deviceInfoData);
+
+        [DllImport("setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern int SetupDiEnumDeviceInterfaces(
-            int deviceInfoSet,
-            int deviceInfoData,
-            ref Guid lpHidGuid,
-            int memberIndex,
-            ref SpDeviceInterfaceData lpDeviceInterfaceData);
+           IntPtr hDevInfo,
+           IntPtr devInfo,
+           ref Guid interfaceClassGuid,
+           UInt32 memberIndex,
+           ref SpDeviceInterfaceData deviceInterfaceData);
 
-        [DllImport("setupapi.dll")]
+        [DllImport("setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern int SetupDiGetDeviceInterfaceDetail(
-            int deviceInfoSet,
-            ref SpDeviceInterfaceData lpDeviceInterfaceData,
-            IntPtr aPtr,
-            int detailSize,
+            IntPtr hDevInfo,
+            ref SpDeviceInterfaceData deviceInterfaceData,
+            ref SpDeviceInterfaceDetailData deviceInterfaceDetailData,
+            int deviceInterfaceDetailDataSize,
+            IntPtr requiredSize,
+            IntPtr deviceInfoData);
+
+        [DllImport("setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int SetupDiGetDeviceInterfaceDetail(
+            IntPtr hDevInfo,
+            ref SpDeviceInterfaceData deviceInterfaceData,
+            IntPtr deviceInterfaceDetailData,
+            int deviceInterfaceDetailDataSize,
             out int requiredSize,
-            IntPtr bPtr);
+            IntPtr deviceInfoData);
+
+        [DllImport("setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr SetupDiGetClassDevs(
+           ref Guid classGuid,
+           IntPtr enumerator,
+           IntPtr hwndParent,
+           int flags);
 
         [DllImport("setupapi.dll")]
-        public static extern int SetupDiGetDeviceInterfaceDetail(
-            int deviceInfoSet,
-            ref SpDeviceInterfaceData lpDeviceInterfaceData,
-            ref PspDeviceInterfaceDetailData interfaceDetailData,
-            int detailSize,
-            ref int requiredSize,
-            IntPtr bPtr);
+        public static extern int SetupDiDestroyDeviceInfoList(IntPtr hDevInfo);
 
         [DllImport("hid.dll")]
         public static extern void HidD_GetHidGuid(out Guid lpHidGuid);
 
-        [DllImport("setupapi.dll")]
-        public static extern int SetupDiGetClassDevs(
-            ref Guid lpHidGuid,
-            IntPtr enumerator,
-            IntPtr hwndParent,
-            int flags);
-
-        [DllImport("kernel32.dll")]
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern IntPtr CreateFile(
             string lpFileName,
-            uint dwDesiredAccess,
-            uint dwShareMode,
+            UInt32 dwDesiredAccess,
+            UInt32 dwShareMode,
             IntPtr lpSecurityAttributes,
-            uint dwCreationDisposition,
-            uint dwFlagsAndAttributes,
-            uint hTemplateFile);
+            UInt32 dwCreationDisposition,
+            UInt32 dwFlagsAndAttributes,
+            IntPtr hTemplateFile);
+    }
 
-        [DllImport("setupapi.dll")]
-        public static extern int SetupDiDestroyDeviceInfoList(int deviceInfoSet);
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SpDevInfoData
+    {
+        public uint cbSize;
+        public Guid ClassGuid;
+        public uint DevInst;
+        public uint Reserved;
     }
 
     [StructLayout(LayoutKind.Sequential)]
@@ -71,12 +99,12 @@ namespace SDRSharp.FUNcube
         public int Flags;
         public int Reserved;
     }
-    
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
-    struct PspDeviceInterfaceDetailData
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    struct SpDeviceInterfaceDetailData
     {
         public int cbSize;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 1024 * 4)]
         public string DevicePath;
     }
 }
--- FUNcube/UsbDevice.cs
+++ FUNcube/UsbDevice.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Runtime.InteropServices;
 using Microsoft.Win32.SafeHandles;
 
 namespace SDRSharp.FUNcube
 {
-    public class UsbDevice
+    public static class UsbDevice
     {
         public static FileStream Open(string deviceId)
         {
@@ -20,40 +21,54 @@ namespace SDRSharp.FUNcube
                 IntPtr.Zero,
                 UsbAPI.DigcfInterfaceDevice | UsbAPI.DigcfPresent);
 
-            var result = -1;
+            var deviceInfoData = new SpDevInfoData();
+            deviceInfoData.cbSize = (uint) Marshal.SizeOf(deviceInfoData);
 
-            while (result != 0)
+            for (var i = 0u; UsbAPI.SetupDiEnumDeviceInfo(hDevInfo, i, ref deviceInfoData); i++)
             {
-                var interfaceData = new SpDeviceInterfaceData();
-                interfaceData.cbSize = Marshal.SizeOf(interfaceData);
-                result = UsbAPI.SetupDiEnumDeviceInterfaces(
-                    hDevInfo,
-                    0,
-                    ref  guid,
-                    0,
-                    ref interfaceData);
+                uint bufferSize = 1024 * 4;
+                var sb = new StringBuilder((int) bufferSize);
+                uint propertyInfoRegDataType;
 
-                int requiredSize;
-                UsbAPI.SetupDiGetDeviceInterfaceDetail(
-                    hDevInfo,
-                    ref interfaceData,
-                    IntPtr.Zero,
-                    0,
-                    out requiredSize,
-                    IntPtr.Zero);
-                
-                var interfaceDetail = new PspDeviceInterfaceDetailData();
-                interfaceDetail.cbSize = 5;
-                UsbAPI.SetupDiGetDeviceInterfaceDetail(
-                    hDevInfo,
-                    ref interfaceData,
-                    ref interfaceDetail,
-                    requiredSize,
-                    ref requiredSize,
-                    IntPtr.Zero);
-                
-                if (interfaceDetail.DevicePath.ToLower().Contains(deviceId))
+                UsbAPI.SetupDiGetDeviceRegistryProperty(
+                        hDevInfo,
+                        ref deviceInfoData,
+                        UsbAPI.SpdrpHardwareId,
+                        out propertyInfoRegDataType,
+                        sb,
+                        bufferSize,
+                        out bufferSize);
+
+                if (sb.ToString().ToLower().Contains(deviceId))
                 {
+                    var interfaceData = new SpDeviceInterfaceData();
+                    interfaceData.cbSize = Marshal.SizeOf(interfaceData);
+                    UsbAPI.SetupDiEnumDeviceInterfaces(
+                        hDevInfo,
+                        IntPtr.Zero,
+                        ref guid,
+                        i,
+                        ref interfaceData);
+
+                    int requiredSize;
+                    UsbAPI.SetupDiGetDeviceInterfaceDetail(
+                        hDevInfo,
+                        ref interfaceData,
+                        IntPtr.Zero,
+                        0,
+                        out requiredSize,
+                        IntPtr.Zero);
+
+                    var interfaceDetail = new SpDeviceInterfaceDetailData();
+                    interfaceDetail.cbSize = IntPtr.Size == 8 ? 8 : 4 + Marshal.SystemDefaultCharSize;
+                    UsbAPI.SetupDiGetDeviceInterfaceDetail(
+                        hDevInfo,
+                        ref interfaceData,
+                        ref interfaceDetail,
+                        requiredSize,
+                        IntPtr.Zero,
+                        IntPtr.Zero);
+
                     var unsafeHandle = UsbAPI.CreateFile(
                         interfaceDetail.DevicePath,
                         UsbAPI.GenericRead | UsbAPI.GenericWrite,
@@ -61,7 +76,7 @@ namespace SDRSharp.FUNcube
                         IntPtr.Zero,
                         UsbAPI.OpenExisting,
                         0,
-                        0);
+                        IntPtr.Zero);
 
                     var hidHandle = new SafeFileHandle(unsafeHandle, true);
 
