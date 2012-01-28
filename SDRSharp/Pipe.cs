ea4fbad7c691013a022eaf6dbf5f41a4ac69c0a2
prog <prog@d3cde5d6-82b8-4669-8b62-43054d6781d8>
2012-01-28 20:29:38 +0000

    Forgotten file
    git-svn-id: https://subversion.assembla.com/svn/sdrsharp/trunk@148 d3cde5d6-82b8-4669-8b62-43054d6781d8

---
+++ SDRSharp/Pipe.cs
@@ -0,0 +1,58 @@
+﻿namespace SDRSharp
+{
+    public class Pipe<T> where T: class
+    {
+        private int _headPtr;
+        private int _tailPtr;
+        private readonly T[] _buffer;
+
+        public Pipe(int capacity)
+        {
+            _buffer = new T[capacity];
+        }
+
+        public void AdvanceRead()
+        {
+            if (_tailPtr == _headPtr)
+            {
+                return;
+            }
+            _tailPtr++;
+            if (_tailPtr >= _buffer.Length)
+            {
+                _tailPtr = 0;
+            }
+        }
+
+        public void AdvanceWrite()
+        {
+            _headPtr++;
+            if (_headPtr >= _buffer.Length)
+            {
+                _headPtr = 0;
+            }
+            if (_headPtr == _tailPtr)
+            {
+                _tailPtr++;
+                if (_tailPtr >= _buffer.Length)
+                {
+                    _tailPtr = 0;
+                }
+            }
+        }
+
+        public T Head
+        {
+            get { return _buffer[_headPtr]; }
+            set { _buffer[_headPtr] = value; }
+        }
+
+        public T Tail
+        {
+            get
+            {
+                return _buffer[_tailPtr];
+            }
+        }
+    }
+}
