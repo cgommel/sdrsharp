5d941725baf050202e739b32f207048136bc9058
prog <prog@d3cde5d6-82b8-4669-8b62-43054d6781d8>
2012-01-28 19:34:10 +0000

    Fixed Mono support. Thanks to Christopher G Sylvain (kb3cs) for pointing out the problem
    git-svn-id: https://subversion.assembla.com/svn/sdrsharp/trunk@147 d3cde5d6-82b8-4669-8b62-43054d6781d8

--- Radio/Fourier.cs
+++ Radio/Fourier.cs
@@ -113,90 +113,87 @@ namespace SDRSharp.Radio
 
 #if MANAGED_ONLY
 
-        private unsafe static void ManagedForwardTransform(Complex[] iq, int length)
+        private static void ManagedForwardTransform(Complex* samples, int length)
         {
-            fixed (Complex* samples = iq)
+            int nm1 = length - 1;
+            int nd2 = length / 2;
+            int i, j, jm1, k, l, m, le, le2, ip, nd4;
+            float ur, ui, sr, si, tr, ti;
+
+            m = 0;
+            i = length;
+            while (i > 1)
             {
-                int nm1 = length - 1;
-                int nd2 = length / 2;
-                int i, j, jm1, k, l, m, le, le2, ip, nd4;
-                float ur, ui, sr, si, tr, ti;
+                ++m;
+                i = (i >> 1);
+            }
 
-                m = 0;
-                i = length;
-                while (i > 1)
+            j = nd2;
+
+            for (i = 1; i < nm1; ++i)
+            {
+                if (i < j)
                 {
-                    ++m;
-                    i = (i >> 1);
+                    tr = samples[j].Real;
+                    ti = samples[j].Imag;
+                    samples[j].Real = samples[i].Real;
+                    samples[j].Imag = samples[i].Imag;
+                    samples[i].Real = tr;
+                    samples[i].Imag = ti;
                 }
 
-                j = nd2;
+                k = nd2;
 
-                for (i = 1; i < nm1; ++i)
+                while (k <= j)
                 {
-                    if (i < j)
+                    j = j - k;
+                    k = k / 2;
+                }
+
+                j += k;
+            }
+
+            for (l = 1; l <= m; ++l)
+            {
+                le = 1 << l;
+                le2 = le / 2;
+                ur = 1;
+                ui = 0;
+
+                sr = (float) Math.Cos(Math.PI / le2);
+                si = (float) -Math.Sin(Math.PI / le2);
+
+                for (j = 1; j <= le2; ++j)
+                {
+                    jm1 = j - 1;
+
+                    for (i = jm1; i <= nm1; i += le)
                     {
-                        tr = samples[j].Real;
-                        ti = samples[j].Imag;
-                        samples[j].Real = samples[i].Real;
-                        samples[j].Imag = samples[i].Imag;
-                        samples[i].Real = tr;
-                        samples[i].Imag = ti;
+                        ip = i + le2;
+                        tr = samples[ip].Real * ur - samples[ip].Imag * ui;
+                        ti = samples[ip].Real * ui + samples[ip].Imag * ur;
+                        samples[ip].Real = samples[i].Real - tr;
+                        samples[ip].Imag = samples[i].Imag - ti;
+                        samples[i].Real = samples[i].Real + tr;
+                        samples[i].Imag = samples[i].Imag + ti;
                     }
 
-                    k = nd2;
-
-                    while (k <= j)
-                    {
-                        j = j - k;
-                        k = k / 2;
-                    }
-
-                    j += k;
+                    tr = ur;
+                    ur = tr * sr - ui * si;
+                    ui = tr * si + ui * sr;
                 }
+            }
 
-                for (l = 1; l <= m; ++l)
-                {
-                    le = 1 << l;
-                    le2 = le / 2;
-                    ur = 1;
-                    ui = 0;
+            nd4 = nd2 / 2;
+            for (i = 0; i < nd4; i++)
+            {
+                Complex tmp = samples[i];
+                samples[i] = samples[nd2 - i - 1];
+                samples[nd2 - i - 1] = tmp;
 
-                    sr = Math.Cos(Math.PI / le2);
-                    si = -Math.Sin(Math.PI / le2);
-
-                    for (j = 1; j <= le2; ++j)
-                    {
-                        jm1 = j - 1;
-
-                        for (i = jm1; i <= nm1; i += le)
-                        {
-                            ip = i + le2;
-                            tr = samples[ip].Real * ur - samples[ip].Imag * ui;
-                            ti = samples[ip].Real * ui + samples[ip].Imag * ur;
-                            samples[ip].Real = samples[i].Real - tr;
-                            samples[ip].Imag = samples[i].Imag - ti;
-                            samples[i].Real = samples[i].Real + tr;
-                            samples[i].Imag = samples[i].Imag + ti;
-                        }
-
-                        tr = ur;
-                        ur = tr * sr - ui * si;
-                        ui = tr * si + ui * sr;
-                    }
-                }
-
-                nd4 = nd2 / 2;
-                for (i = 0; i < nd4; i++)
-                {
-                    Complex tmp = samples[i];
-                    samples[i] = samples[nd2 - i - 1];
-                    samples[nd2 - i - 1] = tmp;
-
-                    tmp = samples[nd2 + i];
-                    samples[nd2 + i] = samples[nd2 + nd2 - i - 1];
-                    samples[nd2 + nd2 - i - 1] = tmp;
-                }
+                tmp = samples[nd2 + i];
+                samples[nd2 + i] = samples[nd2 + nd2 - i - 1];
+                samples[nd2 + nd2 - i - 1] = tmp;
             }
         }
 
