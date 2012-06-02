f745c678e6c12d97c5f6082f31cea08763c3b623
prog <prog@d3cde5d6-82b8-4669-8b62-43054d6781d8>
2012-06-02 16:45:15 +0000

    Added mono friendly startup code
    git-svn-id: https://subversion.assembla.com/svn/sdrsharp/trunk@321 d3cde5d6-82b8-4669-8b62-43054d6781d8

--- SDRSharp/Program.cs
+++ SDRSharp/Program.cs
@@ -10,20 +10,23 @@ namespace SDRSharp
         [STAThread]
         static void Main()
         {
-            var process = Process.GetCurrentProcess();
-            process.PriorityBoostEnabled = true;
-            process.PriorityClass = ProcessPriorityClass.RealTime;
-#if !LINUX
-            Utils.TimeBeginPeriod(1);
-#endif
+            if (Environment.OSVersion.Platform == PlatformID.Win32Windows || Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                var process = Process.GetCurrentProcess();
+                process.PriorityBoostEnabled = true;
+                process.PriorityClass = ProcessPriorityClass.RealTime;
+                Utils.TimeBeginPeriod(1);
+            }
 
             Control.CheckForIllegalCrossThreadCalls = false;
             Application.EnableVisualStyles();
             Application.Run(new MainForm());
-            
-#if !LINUX
-            Utils.TimeEndPeriod(1);
-#endif
+
+            if (Environment.OSVersion.Platform == PlatformID.Win32Windows || Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                Utils.TimeEndPeriod(1);
+            }
+
             Application.Exit(); // ExtIO may have some thread still running preventing the program from terminating
         }
     }
