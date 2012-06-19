56c58a885fd5fd1b1216f2eb8078bdc11dda1f6d
prog <prog@d3cde5d6-82b8-4669-8b62-43054d6781d8>
2012-06-19 07:26:13 +0000

    GUI lifting
    git-svn-id: https://subversion.assembla.com/svn/sdrsharp/trunk@406 d3cde5d6-82b8-4669-8b62-43054d6781d8

--- PanView/SpectrumAnalyzer.cs
+++ PanView/SpectrumAnalyzer.cs
@@ -348,13 +348,13 @@ namespace SDRSharp.PanView
             using (var transparentBackground = new SolidBrush(Color.FromArgb(80, Color.DarkGray)))
             using (var transparentSpot = new SolidBrush(Color.FromArgb(200, Color.Red)))
             using (var redPen = new Pen(Color.Red))
-            using (var yellowPen = new Pen(Color.Yellow))
+            //using (var yellowPen = new Pen(Color.Yellow))
             using (var graphics = Graphics.FromImage(_buffer))
             using (var font = new Font("Arial", 12f))
             {
                 if (cursorWidth < ClientRectangle.Width)
                 {
-                    var carrierPen = _changingFrequency ? yellowPen : redPen;
+                    var carrierPen = redPen;
                     carrierPen.Width = CarrierPenWidth;
                     graphics.FillRectangle(transparentBackground, _lower, 0, bandpassWidth, ClientRectangle.Height);
                     if (xCarrier >= AxisMargin && xCarrier <= ClientRectangle.Width - AxisMargin)
@@ -379,14 +379,14 @@ namespace SDRSharp.PanView
                             graphics.FillEllipse(transparentSpot, _trackingX - 5, newY - 5, 10, 10);
                         }
                     }
-                    var fstring = GetFrequencyDisplay(_changingFrequency ? _frequency : _trackingFrequency);
+                    var fstring = GetFrequencyDisplay(_changingFrequency ? _frequency : _changingBandwidth ? _filterBandwidth : _trackingFrequency);
                     var stringSize = graphics.MeasureString(fstring, font);
                     var xOffset = _trackingX + 15.0f;
                     var yOffset = _trackingY + Cursor.Current.Size.Height - 8.0f;
                     xOffset = Math.Min(xOffset, ClientRectangle.Width - stringSize.Width + 4);
                     yOffset = Math.Min(yOffset, ClientRectangle.Height - stringSize.Height + 1);
                     //graphics.FillRectangle(transparentBackground, xOffset - 4, yOffset - 1, stringSize.Width, stringSize.Height);
-                    graphics.DrawString(fstring, font, _changingFrequency ? Brushes.Yellow : Brushes.White, (int) xOffset, (int) yOffset, StringFormat.GenericTypographic);
+                    graphics.DrawString(fstring, font, Brushes.White, (int) xOffset, (int) yOffset, StringFormat.GenericTypographic);
                 }
             }
         }
