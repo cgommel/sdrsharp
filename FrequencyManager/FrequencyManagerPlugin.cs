44522fc79846084743ee67a3d851b18798a7c427
prog <prog@d3cde5d6-82b8-4669-8b62-43054d6781d8>
2012-08-25 23:28:07 +0000

    Added a missing file
    git-svn-id: https://subversion.assembla.com/svn/sdrsharp/trunk@595 d3cde5d6-82b8-4669-8b62-43054d6781d8

---
+++ FrequencyManager/FrequencyManagerPlugin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+using SDRSharp.Common;
+
+
+namespace SDRSharp.FrequencyManager
+{
+    public class FrequencyManagerPlugin: ISharpPlugin
+    {
+        private const string _displayName = "Frequency Manager";
+        private ISharpControl _controlInterface;
+        private FrequencyManagerPanel _frequencyManagerPanel;
+
+        public bool HasGui
+        {
+            get { return true; }
+        }
+
+        public UserControl GuiControl
+        {
+            get { return _frequencyManagerPanel; }
+        }
+
+        public string DisplayName
+        {
+            get { return _displayName; }
+        }
+
+        public void Closing()
+        {
+            
+        }
+
+        public void Initialise(ISharpControl control)
+        {
+            _controlInterface = control;
+            _frequencyManagerPanel = new FrequencyManagerPanel(_controlInterface); 
+        }
+    }
+}
