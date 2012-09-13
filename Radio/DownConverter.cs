82c0d666aa614ad897e23d9eccc1e62776d0e9c3
prog <prog@d3cde5d6-82b8-4669-8b62-43054d6781d8>
2012-09-13 20:21:46 +0000

    Forgotten file
    git-svn-id: https://subversion.assembla.com/svn/sdrsharp/trunk@715 d3cde5d6-82b8-4669-8b62-43054d6781d8

---
+++ Radio/DownConverter.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Threading;
+
+namespace SDRSharp.Radio
+{
+    public unsafe sealed class DownConverter
+    {
+        private readonly Oscillator[] _oscillators;
+        private readonly AutoResetEvent _event = new AutoResetEvent(false);
+
+        private bool _initialized;
+        private double _sampleRate;
+        private double _frequency;
+        private int _completedCount;
+
+        public DownConverter(int phaseCount)
+        {
+            _oscillators = new Oscillator[phaseCount];
+            for (var i = 0; i < phaseCount; i++)
+            {
+                _oscillators[i] = new Oscillator();
+            }
+        }
+
+        public DownConverter() : this(1) // this(Environment.ProcessorCount)
+        {
+        }
+
+        public double SampleRate
+        {
+            get { return _sampleRate; }
+            set
+            {
+                if (_sampleRate != value)
+                {
+                    _sampleRate = value;
+                    _initialized = false;
+                }
+            }
+        }
+
+        public double Frequency
+        {
+            get { return _frequency; }
+            set
+            {
+                if (_frequency != value)
+                {
+                    _frequency = value;
+                    SetFrequency();
+                }
+            }
+        }
+
+        private void Initialize()
+        {
+            var targetAngularFrequency = 2.0 * Math.PI * _frequency / _sampleRate;
+
+            var targetSin = Math.Sin(targetAngularFrequency);
+            var targetCos = Math.Cos(targetAngularFrequency);
+
+            var vectR = 1.0;
+            var vectI = 0.0;
+
+            for (var i = 0; i < _oscillators.Length; i++)
+            {
+                _oscillators[i].SampleRate = _sampleRate;
+                _oscillators[i].StateReal = vectR;
+                _oscillators[i].StateImag = vectI;
+
+                var outR = vectR * targetCos - vectI * targetSin;
+                var outI = vectI * targetCos + vectR * targetSin;
+                var oscGn = 1.95 - (vectR * vectR + vectI * vectI);
+                vectR = oscGn * outR;
+                vectI = oscGn * outI;
+            }
+        }
+
+        public void SetFrequency()
+        {
+            var threadFrequency = _frequency * _oscillators.Length;
+
+            for (var i = 0; i < _oscillators.Length; i++)
+            {
+                _oscillators[i].Frequency = threadFrequency;
+            }
+        }
+
+        public void Process(Complex* buffer, int length)
+        {
+            if (!_initialized)
+            {
+                Initialize();
+                _initialized = true;
+            }
+
+            _completedCount = 0;
+
+            for (var i = 1; i < _oscillators.Length; i++)
+            {
+                DSPThreadPool.QueueUserWorkItem(
+                    delegate (object parameter)
+                        {
+                            var index = (int) parameter;
+                            _oscillators[index].Mix(buffer, length, index, _oscillators.Length);
+                            Interlocked.Increment(ref _completedCount);
+                            _event.Set();
+                        }, i);
+            }
+
+            _oscillators[0].Mix(buffer, length, 0, _oscillators.Length);
+            Interlocked.Increment(ref _completedCount);
+
+            while (_completedCount < _oscillators.Length)
+            {
+                _event.WaitOne();
+            }
+        }
+    }
+}
