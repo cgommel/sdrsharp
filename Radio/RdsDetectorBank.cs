e826d3e2b13a69cc5b9ec9aec82bd3f9efef832e
prog <prog@d3cde5d6-82b8-4669-8b62-43054d6781d8>
2013-08-18 18:01:03 +0000

    Merged Ian's forward error correction (FEC) for RDS. The option can be switched on and off from the configuration file. PS: RDS is a flawy spec.
    git-svn-id: https://subversion.assembla.com/svn/sdrsharp/trunk@1151 d3cde5d6-82b8-4669-8b62-43054d6781d8

--- Radio/RdsDetectorBank.cs
+++ Radio/RdsDetectorBank.cs
@@ -4,8 +4,8 @@ namespace SDRSharp.Radio
 {
     public class RdsDetectorBank
     {
-        private readonly RdsDumpGroups _dumpGroups = new RdsDumpGroups();
-        private readonly SyndromeDetector[] _detectors = new SyndromeDetector[26];
+        private readonly RdsDumpGroups _dumpGroups;
+        private readonly SyndromeDetector _detector;
 
         public string RadioText
         {
@@ -24,18 +24,13 @@ namespace SDRSharp.Radio
 
         public RdsDetectorBank()
         {
-            for (var i = 0; i < _detectors.Length; i++)
-            {
-                _detectors[i] = new SyndromeDetector(i, _dumpGroups);
-            }
+            _dumpGroups = new RdsDumpGroups();
+            _detector = new SyndromeDetector(_dumpGroups);
         }
 
         public void Process(bool b)
         {
-            for (var i = 0; i < _detectors.Length; i++)
-            {
-                _detectors[i].Clock(b);
-            }
+            _detector.Clock(b);
         }
 
         public void Reset()
@@ -44,119 +39,189 @@ namespace SDRSharp.Radio
         }
     }
 
-    public class SyndromeDetector : GroupSequencer
+    public class SyndromeDetector
     {
-        private readonly RdsDumpGroups _dumpGroups;
-        private UInt16 _syndrome;
-        private int _state;
-
-        public SyndromeDetector(int state, RdsDumpGroups dumpGroups)
+        [Flags]
+        protected enum BlockSequence
         {
-            _state = state;
+            WaitBitSync,
+            GotBitSync,
+            GotA,
+            GotB,
+            GotC,
+            GotD
+        }
+
+        private BlockSequence _sequence = BlockSequence.WaitBitSync;
+        private UInt32 _raw;
+        private readonly RdsDumpGroups _dumpGroups;
+        private readonly bool _useFec = Utils.GetBooleanSetting("rdsUseFEC");
+        private UInt16 _syndrome;
+        private int _count;
+
+        public SyndromeDetector(RdsDumpGroups dumpGroups)
+        {
             _dumpGroups = dumpGroups;
         }
 
-        public bool Clock(bool b)
+        public void Clock(bool b)
         {
-            var result = false;
-            _state++;
-            if (_state == 26)
+            _raw <<= 1;
+            _raw |= b ? 1u : 0u;
+            _count++;
+
+            if (_sequence == BlockSequence.WaitBitSync)
             {
-                if (Sequence(_syndrome) == BlockSequence.Live)
+                _syndrome = BuildSyndrome(_raw);
+                _syndrome ^= 0x3d8;
+                _sequence = _syndrome == 0 ? BlockSequence.GotA : BlockSequence.WaitBitSync;
+                Block1 = (UInt16)((_raw >> 10) & 0xffff);
+                _count = 0;
+            }
+
+            if (_count == 26)
+            {
+                ProcessSyndrome();
+                if (_sequence == BlockSequence.GotD)
                 {
-                    result = _dumpGroups.AnalyseFrames(Block1, Block2, Block3, Block4);
+                    _dumpGroups.AnalyseFrames(Block1, Block2, Block3, Block4);
+                    _sequence = BlockSequence.GotBitSync;
                 }
-                _syndrome = 0;
-                _state = 0;
+
+                _count = 0;
             }
+        }
 
-            _raw = (_raw << 1);
-
-            if (b)
+        private void ProcessSyndrome()
+        {
+            _syndrome = BuildSyndrome(_raw);
+            switch (_sequence)
             {
-                _raw++;
+                case BlockSequence.GotBitSync:
+                    _syndrome ^= 0x3d8;
+                    _sequence = BlockSequence.GotA;
+                    break;
+                case BlockSequence.GotA:
+                    _syndrome ^= 0x3d4;
+                    _sequence = BlockSequence.GotB;
+                    break;
+                case BlockSequence.GotB:
+                    _syndrome ^= (UInt16)(Block2 & 0x800) == 0 ? (UInt16)0x25c : (UInt16)0x3cc;
+                    _sequence = BlockSequence.GotC;
+                    break;
+                case BlockSequence.GotC:
+                    _syndrome ^= 0x258;
+                    _sequence = BlockSequence.GotD;
+                    break;
             }
-
-            if (_state < 16)
+            if (_syndrome != 0)
             {
-                int gateA = ((_syndrome & 0x200) != 0) ^ b ? 0x1b9 : 0;
-                _syndrome = (UInt16)((_syndrome << 1) ^ gateA);
+                if (_useFec)
+                {
+                    var corrected = ApplyFEC();
+                    if (_syndrome != 0 || corrected > 2)
+                    {
+                        _sequence = BlockSequence.WaitBitSync;
+                    }
+                    else
+                    {
+                        SetBlock(_sequence, (UInt16)(_raw & 0xffff));
+                    }
+                }
+                else
+                {
+                    _sequence = BlockSequence.WaitBitSync;
+                }
             }
             else
             {
-                int gateB = ((_syndrome & 0x200) != 0) ^ b ? 1 : 0;
-                _syndrome = (UInt16)((_syndrome << 1) ^ gateB);
+                SetBlock(_sequence, (UInt16)((_raw >> 10) & 0xffff));
+            }
+        }
+
+        private void SetBlock(BlockSequence sequence, UInt16 val)
+        {
+            switch (_sequence)
+            {
+                case BlockSequence.GotA:
+                    Block1 = val;
+                    break;
+                case BlockSequence.GotB:
+                    Block2 = val;
+                    break;
+                case BlockSequence.GotC:
+                    Block3 = val;
+                    break;
+                case BlockSequence.GotD:
+                    Block4 = val;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private int ApplyFEC()
+        {
+            const int poly = 0x5b9;
+            const int errorMask = (1 << 5);
+
+            var correction = (uint)(1 << 25);
+            var correctedBitsCount = 0;
+
+            _raw >>= 10;
+            for (var i = 0; i < 16; i++)
+            {
+                var st = ((_syndrome & 0x200) == 0x200);
+                var bitError = (_syndrome & errorMask) == 0;
+                _raw ^= (st && bitError) ? correction : 0;
+                _syndrome <<= 1;
+                _syndrome ^= (UInt16)((st && !bitError) ? poly : 0);
+                correctedBitsCount += (st && bitError) ? 1 : 0;
+                correction >>= 1;
             }
 
-            return result;
+            _syndrome &= 0x3ff;
+            return correctedBitsCount;
+        }
+
+        private static UInt16 BuildSyndrome(uint raw)
+        {
+            UInt16[] Parity = 
+            {
+                0x2DC,
+                0x16E,
+                0x0B7,
+                0x287,
+                0x39F,
+                0x313,
+                0x355,
+                0x376,
+                0x1BB,
+                0x201,
+                0x3DC,
+                0x1EE,
+                0x0F7,
+                0x2A7,
+                0x38F,
+                0x31B
+            };
+
+            var block = raw & 0x3FFFFFF;
+            var syndrome = (UInt16)(block >> 16);
+            for (var i = 0; i < 16; i++)
+            {
+                syndrome ^= (UInt16)(((block & 0x8000) == 0x8000) ? Parity[i] : 0);
+                block <<= 1;
+            }
+
+            return syndrome;
         }
 
         public override string ToString()
         {
-            return string.Format("st {0:d02} sy {1:x04} r {2:x08}", _state, _syndrome, _raw);
+            return string.Format("st {0:d02} sy {1:x04} r {2:x08}", _count, _syndrome, _raw);
         }
-    }
 
-    public class GroupSequencer 
-    {
-        protected enum BlockSequence
-        {
-            Broke = 0,
-            GotA,
-            GotB,
-            GotC,
-            Live
-        } ;
-
-        private BlockSequence _seq = BlockSequence.Broke;
-
-        protected UInt32 _raw;
-
-        protected BlockSequence Sequence(UInt16 syndrome)
-        {
-            if ((syndrome & 0x03ff) == 0x0fc)
-            {
-                //A              
-                Block1 = (UInt16)((_raw >> 10) & 0xffff);
-                _seq = BlockSequence.GotA;
-                return _seq;
-            }
-
-            if (((syndrome & 0x03ff) == 0x198) && (_seq == BlockSequence.GotA))
-            {
-                //B             
-                Block2 = (UInt16)((_raw >> 10) & 0xffff);
-                _seq = BlockSequence.GotB;
-                return _seq;
-            }
-
-            if (((syndrome & 0x03ff) == 0x168) && (_seq == BlockSequence.GotB))
-            {
-                //C             
-                Block3 = (UInt16)((_raw >> 10) & 0xffff);
-                _seq = BlockSequence.GotC;
-                return _seq;
-            }
-
-            if (((syndrome & 0x03ff) == 0x350) && (_seq == BlockSequence.GotB))
-            {
-                // c                
-                Block3 = (UInt16)((_raw >> 10) & 0xffff);
-                _seq = BlockSequence.GotC;
-                return _seq;
-            }
-
-            if (((syndrome & 0x03ff) == 0x1b4) && (_seq == BlockSequence.GotC))
-            {
-                //D                
-                Block4 = (UInt16)((_raw >> 10) & 0xffff);
-                _seq = BlockSequence.Live;
-                return _seq;
-            }
-            _seq = BlockSequence.Broke;
-            return _seq;
-        }
-    
         public string GroupCode
         {
             get
@@ -169,22 +234,26 @@ namespace SDRSharp.Radio
 
         public ushort Block1
         {
-            get; private set;
+            get;
+            private set;
         }
 
         public ushort Block2
         {
-            get; private set;
+            get;
+            private set;
         }
 
         public ushort Block3
         {
-            get; private set;
+            get;
+            private set;
         }
 
         public ushort Block4
         {
-            get; private set;
+            get;
+            private set;
         }
     }
 }
